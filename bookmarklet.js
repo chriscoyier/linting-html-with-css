f81e83c55f30b4d93992302852d17f0fb3afdab4
Paul Esch-Laurent <herppfel@gmail.com>
2017-03-12 23:52:49 -0400

    Add bookmarklet

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,17 +1,6 @@
-- Ire Aderinokun's concept: https://bitsofco.de/linting-html-using-css/
-- Demo page: https://chriscoyier.github.io/linting-html-with-css/
-
-### Bookmarklet
-
-```js
 javascript:(function() {
   var link = document.createElement("link");
   link.rel = "stylesheet";
   link.href = "https://cdn.rawgit.com/chriscoyier/linting-html-with-css/master/lint.css";
   document.getElementsByTagName("head")[0].appendChild(link);
 })();
-```
-
-### Ideas:
-
-- Browser extension to inject the CSS
